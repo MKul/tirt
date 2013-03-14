d9ec46359df5b8cd4969e15820d85ff6676563e5
daightron@gmail.com <daightron@gmail.com@8bf71321-dc60-b1e5-85b7-93aea43bf0f2>
2013-03-14 07:45:42 +0000

    Propozycja klasy Bts, zobaczcie czy czegoś nie zmienić albo poprawić.

---
+++ trunk/Tirt_MMS/src/Bts.java
@@ -0,0 +1,50 @@
+
+public class Bts {
+	
+	private int x;
+	private int y;
+	private int range;
+	private int performence;
+	
+	public Bts(int posX, int posY){
+		x=posX;
+		y=posY;
+	}
+	
+	public Bts(int posX, int posY, int range){
+		x=posX;
+		y=posY;
+		this.range=range;
+	}
+	
+	public Bts(int posX, int posY, int range, int perf){
+		x=posX;
+		y=posY;
+		this.range=range;
+		perf=performence;
+	}
+	
+	public void setRange(int r){
+		range=r;
+	}
+	
+	public void setPerformance(int p){
+		performence=p;
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public int getRange(){
+		return range;
+	}
+	
+	public int getPerformance(){
+		return performence;
+	}
+}
