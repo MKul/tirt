552fdf3cf18652a610ed4626ff6931ff4e277d03
daightron@gmail.com <daightron@gmail.com@8bf71321-dc60-b1e5-85b7-93aea43bf0f2>
2013-03-14 07:49:41 +0000

    Propozycja User'a. To samo co przy Bts

---
+++ Tirt_MMS/src/User.java
@@ -0,0 +1,31 @@
+
+//author: Marcin Kulisiewicz
+
+public class User {
+	
+	private int x;
+	private int y;
+	
+	public User(int posX, int posY){
+		x=posX;
+		y=posY;
+	}
+	
+	public void setX(int newX){
+		x=newX;
+	}
+	
+	public void setY(int newY){
+		y=newY;
+	}
+	
+	public int getX(){
+		return x;
+	}
+		
+	public int getY(){
+		return y;
+	}
+	
+	
+}
